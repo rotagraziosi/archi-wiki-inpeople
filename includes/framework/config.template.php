d25d02ffd547dcf4914135f9ffbc423f57aad707
Pierre Rudloff <contact@rudloff.pro>
2013-03-14 10:27:20 +0000

    Exemple de fichier de configuration

---
+++ includes/framework/config.template.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Paramètres du site
+ * 
+ * PHP Version 5.3.3
+ * 
+ * @category Class
+ * @package  ArchiWiki
+ * @author   Pierre Rudloff <contact@rudloff.pro>
+ * @license  Inconnue https://archi-strasbourg.org/?archiAffichage=faq
+ * @link     https://archi-strasbourg.org/
+ * 
+ * */
+$this->bdd_host     = "";
+$this->bdd_user     = "";
+$this->bdd_password = "";
+$this->bdd_name     = "";
+
+// Pour l'identification unique d'un utilisateur dans la session
+$this->idSite = ""; 
+
+//Mot de passe universel
+$this->adminPass=="";
+
+$this->mail     = "";
+$this->authorLink = "";
+$this->tradLink = "";
+
+$this->titreSite = "";
+$this->descSite=_("");
+
+?>
