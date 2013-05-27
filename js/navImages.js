5ce93186cb038c0491e66840ea2584d16b9570ba
Pierre Rudloff <contact@rudloff.pro>
2013-05-27 23:23:54 +0000

    Mode plein écran

--- js/navImages.js
+++ js/navImages.js
@@ -14,8 +14,34 @@ var navImages = function (e) {
         window.location = dest.getAttribute('href');
     }
 };
+
+var fullscreen = function () {
+    'use strict';
+    var img = document.getElementById('imageAfficheeID');
+    img.src = img.src.replace('grand', 'originaux');
+    if (img.requestFullscreen) {
+        img.requestFullscreen();
+    } else if (img.mozRequestFullScreen) {
+        img.mozRequestFullScreen();
+    } else if (img.webkitRequestFullscreen) {
+        img.webkitRequestFullscreen();
+    }
+};
+
+var exitFullscreen = function () {
+    'use strict';
+    if (!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement) {
+        var img = document.getElementById('imageAfficheeID');
+        img.src = img.src.replace('originaux', 'grand');
+    }
+};
+
 var initNavImages = function () {
     'use strict';
     window.addEventListener('keydown', navImages);
+    document.addEventListener('mozfullscreenchange', exitFullscreen);
+    document.addEventListener('webkitfullscreenchange', exitFullscreen);
+    document.addEventListener('fullscreenchange', exitFullscreen);
+    document.getElementById('imageAfficheeID').addEventListener('click', fullscreen);
 };
 window.addEventListener('load', initNavImages);
