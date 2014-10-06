a1a462de657b9d532bdd6188403d36e967d29eaf
Pierre Rudloff <contact@rudloff.pro>
2014-10-06 19:21:30 +0200

    Exemple de configuration plus explicite

--- includes/framework/config.template.php
+++ includes/framework/config.template.php
@@ -11,10 +11,10 @@
  * @link     https://archi-strasbourg.org/
  * 
  * */
-$this->bdd_host     = "";
+$this->bdd_host     = "localhost";
 $this->bdd_user     = "";
 $this->bdd_password = "";
-$this->bdd_name     = "";
+$this->bdd_name     = "archi-strasbourg";
 
 // Pour l'identification unique d'un utilisateur dans la session
 $this->idSite = ""; 
@@ -22,11 +22,15 @@ $this->idSite = "";
 //Mot de passe universel
 $this->adminPass=="";
 
-$this->mail     = "";
-$this->authorLink = "";
-$this->tradLink = "";
+$this->mail     = "contact@archi-strasbourg.org";
+$this->authorLink = "https://plus.google.com/105153588863548583248/posts";
+$this->tradLink = "http://archi-strasbourg.org:8080/projects/archi/";
 
-$this->titreSite = "";
-$this->descSite=_("");
+$this->titreSite = "Archi-Strasbourg.org";
+$this->descSite=_("Architecture, photos et patrimoine de Strasbourg.");
+$this->langs=array('fr_FR', 'de_DE', 'en_US');
+
+//Pour le plugin reCaptcha
+$this->captchakey='';
 
 ?>
